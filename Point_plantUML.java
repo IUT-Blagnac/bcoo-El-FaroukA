12c51502ce881f3e97df2e49b75292845256696e
El-FaroukA <77385713+El-FaroukA@users.noreply.github.com>
2021-02-03 13:42:06 -0100

    Fix 0.5 : UML ligne

---
+++ Point_plantUML.java
@@ -0,0 +1,19 @@
+@startuml Titre
+'--------------------------------
+' Parametres pour le dessin
+'--------------------------------
+hide circle
+hide empty members
+hide empty methods
+'--------------------------------
+
+class Point {
+x
+y
+}
+
+class Ligne {
+   1erPoint
+   2ePoint
+}
+@enduml
